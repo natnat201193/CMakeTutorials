de42f3f0756e5cd1abf8f142b314ad3f6ad80756
natnat <33712044+natnat201193@users.noreply.github.com>
2021-05-03 11:19:53 +0100

    CMake Tutorial EP 4 | Versioning Source Code

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <adder.h>
 #include <GLFW/glfw3.h>
+#include <OLASConfig.h>
 // CmakeTest
 
-int main()
+int main(int argc, char* argv[])
 {
-    std::cout << "Hey, Zues!\n";
+    std::cout << argv[0] << "\nOLAS Version: " << OLAS_VERSION_MAJOR << "." << OLAS_VERSION_MINOR << "\n";
 
+    std::cout << "Hey, Zues!\n";
     std::cout << add(72.1f, 73.8f) << '\n';
 
     GLFWwindow *window;
